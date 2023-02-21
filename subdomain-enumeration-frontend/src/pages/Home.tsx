ada05692a34f098e3b18d5666e81867317bf9a10
Your Name <you@example.com>
2023-02-21 16:35:11 +0530

    home page desktop view

--- subdomain-enumeration-frontend/src/pages/Home.tsx
+++ subdomain-enumeration-frontend/src/pages/Home.tsx
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SearchBar from "../components/SearchBar";
 import "./home.css";
 
 const Home = () => {
+
   return (
-    <>
+    <div className="header-container">
+
       <div className="home-title">
         <h1>
           Say
           <span style={{ textTransform: "uppercase" }}>goodbye</span>
           <span>
-            to <span style={{ textTransform: "uppercase" }}>cli</span>
+            to <span style={{ textTransform: "uppercase" }}>cli.</span>
           </span>
         </h1>
       </div>
-      {/* <SearchBar /> */}
-    </>
+
+
+      <p className="below-header pcol2">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, harum consequatur, ipsam, odit commodi aut magni Lorem ipsum dolor sit, amet consectetur adipisicing.
+      </p>
+
+      <p className="below-header pcol1">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, praesentium?Lorem ipsum dolor sit amet.</p>
+
+      <SearchBar />
+    </div>
   );
 };
 
