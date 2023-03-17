65c0b43efcedb9cbaf499bab5c40cb816c12b403
Anudeep <anudeep.22cs@kct.ac.in>
2023-03-17 19:39:22 +0530

    removed unwanted files

--- subdomain-enumeration-frontend/src/context/UseSubdomainContext.tsx
+++ subdomain-enumeration-frontend/src/context/UseSubdomainContext.tsx
@@ -1,9 +1,8 @@
 import { useContext } from "react";
 import { subdomainContext } from "./subdomainContext";
-import { StateSubdomainsContext } from "../types/StateSubdomainsContext";
 
 export const UseSubdomainContext = () => {
-  const subdomainData = useContext(subdomainContext) as StateSubdomainsContext;
+  const subdomainData = useContext(subdomainContext);
 
   return subdomainData;
 };
