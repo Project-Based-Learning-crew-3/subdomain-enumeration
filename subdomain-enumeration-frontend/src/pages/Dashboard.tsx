71a72cb667541bcb21a05d6b04d53e60af1faa50
Kevin michael <akevinmichael@gmail.com>
2023-03-20 22:01:30 +0530

    Complete Dashboard

--- subdomain-enumeration-frontend/src/pages/Dashboard.tsx
+++ subdomain-enumeration-frontend/src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import kctLogo from "../assets/kct.svg";
 import pieChart from "../assets/pieChart.svg";
 import { UseSubdomainContext } from "../context/UseSubdomainContext";
@@ -6,9 +6,15 @@ import { UseSubdomainContext } from "../context/UseSubdomainContext";
 let subdomainsFound: number = 0;
 const Dashboard = () => {
   const { subdomains } = UseSubdomainContext();
-
+  const [statusCode,setStatusCode] = useState({
+    success:0,
+    userErr:0,
+    serverErr:0
+  })
   console.log(subdomains);
   return (
+    <div style={{position:"relative",}}>
+
     <div
       style={{
         color: "white",
@@ -19,29 +25,56 @@ const Dashboard = () => {
       }}
     >
       <img src={kctLogo} alt="logo" width="300px" height="300px" />
-      <h2 className="dashboardTitle" style={{ fontSize: "58px", marginLeft: "2rem",fontFamily:"Raleway",fontWeight:"500"}}>
+      <div style={{display:"flex",flexDirection:"column",justifyContent:"flex-start",color:"white",marginBottom:"3rem"}}>
+      <div>
+        <h2 className="dashboardTitle" style={{ fontSize: "58px", marginLeft: "2rem",fontFamily:"Raleway",fontWeight:"500",marginBottom:"0px"}}>
         Subdomains <br></br> found {subdomainsFound}{" "}
-      </h2>
+        </h2>
+      </div>
+      <div style={{display:"flex",flexDirection:"row",justifyContent:"flex-start",marginLeft:"2rem"}}>
+      <div style={{paddingRight:"1.5rem"}}>
+          <p className="success">200-{statusCode.success}</p>
+        </div>
+        <div style={{paddingRight:"1.5rem"}}>
+          <p className="clientError">400-{statusCode.userErr}</p>
+        </div>
+        <div style={{paddingRight:"1.5rem"}}>
+          <p className="serverError">500-{statusCode.serverErr}</p>
+        </div>
+      </div>
+      </div>
       
       <div>
         <img src={pieChart} alt="" />
       </div>
-      <div className="StatusCode">
-        <button
-          type="submit"
-          style={{
-            width: "20%",
-            border: "10% solid gray",
-            borderRadius: "15px",
-            padding: "8px",
-            backgroundColor:"#ACC319",
-            color:"#000000"
-          }}
-        >
-          NEXT
-        </button>
+      
       </div>
-    </div>
+
+<div className="StatusCode">
+<button
+  type="submit"
+  style={{
+    width: "7%",
+    border: "10% solid gray",
+    borderRadius: "15px",
+    padding: "8px",
+    backgroundColor:"#ACC319",
+    fontFamily:"Raleway",
+    fontWeight:"500",
+    fontSize:"1rem",
+    textAlign:"center",
+    color:"#000000",
+    position:"absolute",
+    bottom:"60px",
+    right:"10px",
+    left:"1100px"
+  }}
+>
+  NEXT
+</button>
+</div>
+</div>
+
   );
 };
 
