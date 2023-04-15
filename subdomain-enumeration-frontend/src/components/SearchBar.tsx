66de9228412361e16619627a48812dfbe4e1d122
Anudeep <anudeep.22cs@kct.ac.in>
2023-04-15 14:03:13 +0530

    expose api url

--- subdomain-enumeration-frontend/src/components/SearchBar.tsx
+++ subdomain-enumeration-frontend/src/components/SearchBar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { getLogo } from "../helpers/getCompanyLogo";
 import { getSubdomains } from "../api/getsubdomains";
